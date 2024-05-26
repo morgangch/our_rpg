fa94fca86ab03e032ef7285a081792408ded1eb8
lilyan-bouyer <lilyan.bouyer@epitech.eu>
2024-05-26 17:47:14 +0200

    fix compil error.

--- src/fights/calculate_dmg.c
+++ src/fights/calculate_dmg.c
@@ -8,24 +8,6 @@
 #include "my.h"
 #include "structures.h"
 
-static char *read_item(int item_id)
-{
-    int fd = open("items.txt", O_RDONLY);
-    char *buffer = malloc(sizeof(char) * 1000);
-    char **items;
-
-    if (fd == -1)
-        return NULL;
-    read(fd, buffer, 1000);
-    items = str_to_word_array(buffer, '\n');
-    for (int i = 0; items[i]; i++){
-        if (atoi(items[i]) == item_id){
-            return items[i];
-        }
-    }
-    return NULL;
-}
-
 static char *get_spell(int spell_id)
 {
     int fd = open("spells.txt", O_RDONLY);
